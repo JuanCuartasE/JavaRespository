88b542dbc4749c2a36f961f9a38922f4cf6754f8
Juan Cuartas <juan_cuartas23231@elpoli.edu.co>
2026-01-07 10:14:47 -0500

    feat: Add AuthController for user signup/login and RequestController for managing user requests.

--- backend/backend/src/main/java/com/paradigmaslab/backend/infrastructure/controllers/AuthController.java
+++ backend/backend/src/main/java/com/paradigmaslab/backend/infrastructure/controllers/AuthController.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
 public class AuthController {
     private final AuthUseCase authUseCase;
 
--- backend/backend/src/main/java/com/paradigmaslab/backend/infrastructure/controllers/RequestController.java
+++ backend/backend/src/main/java/com/paradigmaslab/backend/infrastructure/controllers/RequestController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/requests")
-@CrossOrigin(origins = "*") // Allow for local development
 public class RequestController {
     private final RequestUseCase requestUseCase;
 
